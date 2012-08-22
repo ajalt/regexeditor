800a72d6731daf036f034df63a094504cbb9fca2
ajalt <aj@alt>
2012-08-22 14:12:18 -0400

    Added Match and Match All functionality

--- editor.py
+++ editor.py
@@ -1,8 +1,11 @@
 import re
 import sre_constants
-import sys 
+import sys
+import cgi
 from PySide import QtCore, QtGui
 
+def escape(text):
+    return cgi.escape(text).replace('\n', '<br />')
 
 class WindowPane(QtGui.QFrame):
     def __init__(self, title, widget):
@@ -22,6 +25,28 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         
+        #create the top toolbar
+        self.toolbar = QtGui.QToolBar()
+        self.toolbar.setFloatable(False)
+        self.toolbar.setMovable(False)
+        
+        self.button_group = QtGui.QButtonGroup()
+        
+        def create_button(name):
+            b = QtGui.QPushButton(name)
+            b.setCheckable(True)
+            self.button_group.addButton(b)
+            self.toolbar.addWidget(b)
+            return b
+        
+        self.match_button = create_button('Match')
+        self.match_all_button = create_button('Match All')
+        
+        self.match_button.setChecked(True)
+        self.button_group.buttonClicked.connect(lambda id: self.highlight())
+        
+        self.addToolBar(self.toolbar)
+        
         self.central_widget = QtGui.QSplitter(QtCore.Qt.Vertical)
         self.expression_edit = QtGui.QTextEdit()
         self.expression_edit.setAcceptRichText(False)
@@ -37,23 +62,42 @@ class MainWindow(QtGui.QMainWindow):
         
         
         self.setCentralWidget(self.central_widget)
-
+        
+        self.highlight_enabled = True
+        
+    def set_document_html(self, html):
+        self.expression_edit.textChanged.disconnect(self.highlight)
+        self.search_text_edit.document().setHtml(html)
+        self.expression_edit.textChanged.connect(self.highlight)
+        
     def highlight(self):
-        #for match in re.finditer(self.expression_edit.toPlainText(), self.search_text_edit.toPlainText()):
-        
-        pattern = self.expression_edit.document().toPlainText()
         text = self.search_text_edit.document().toPlainText()
-        try:
-            match = re.search(pattern, text)
-        except sre_constants.error as err:
-            pass
-        else:
-            if match:
-                self.search_text_edit.document().setHtml(self.highlight_range(text, match.start(), match.end()))
-            
-    def highlight_range(self, html, start, stop):
-        return html[:start] + '<span style="background-color:#62e55f;">' + html[start:stop] + "</span>" + html[stop:]
+        pattern = self.expression_edit.document().toPlainText()
         
+        if not pattern or not self.highlight_enabled:
+            return
+        
+        html = '<html>'
+        index = 0
+        
+        # Disable highlighting so that this function isn't called recursively
+        # when it sets the document html
+        self.highlight_enabled = False
+        
+        try:
+            for match in re.finditer(pattern, text):
+                html += escape(text[index:match.start()])
+                html += '<span style="background-color:#62e55f;">' + text[match.start():match.end()] + '</span>'
+                index = match.end()
+                if self.button_group.checkedButton() is self.match_button:
+                    break
+            html += escape(text[index:]) + '</html>'
+            self.set_document_html(html)
+        except sre_constants.error as err:
+            print err
+        finally:
+            self.highlight_enabled = True
+            
 app = QtGui.QApplication(sys.argv)
 
 window = MainWindow()
